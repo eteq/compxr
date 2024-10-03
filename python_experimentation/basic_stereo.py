7c1c455d1a3b5e4fc2ea14ca22744fc7bc29919a
Erik Tollerud <erik.tollerud@gmail.com>
2024-10-03 16:54:21 -0400

    sbs stereo better with viewport

--- python_experimentation/basic_stereo.py
+++ python_experimentation/basic_stereo.py
@@ -50,6 +50,9 @@ class BasicStereoWindow(mglw.WindowConfig):
         
         self.program = self.ctx.program(vertex_shader=self.vtx_shader_src, fragment_shader=self.frag_shader_src)
         
+        self.program['scale'] = (1., 1., 1.)
+        self.program['offset'] = (0., 0., 0.)
+
         self.vao = self.ctx.vertex_array(self.program, [])
         self.vao.vertices = 3
         
@@ -60,22 +63,26 @@ class BasicStereoWindow(mglw.WindowConfig):
         self.ctx.enable(self.ctx.DEPTH_TEST)
 
         eyesep = 0.1
-        z = math.sin(time*2*math.pi)*.1 + 1 # .9 to 1.1 
+        z = math.sin(time*2*math.pi)*.8 + 1 # .9 to 1.1 
 
         proj = glm.perspective(math.radians(90), 1.0, 0.1, 1000.0)
 
         lookl = glm.lookAt((-eyesep,0, z), (0.0, 0.0, 0.0), (0.0, 0.0, 1.0))
         lookr = glm.lookAt((eyesep,0, z), (0.0, 0.0, 0.0), (0.0, 0.0, -1.0))
 
+        self.ctx.viewport = (0, 0, self.window_size[0]//2, self.window_size[1])
         self.program['camera'].write(proj*lookl)
-        self.program['scale'] = (0.5, 1., 1.)
-        self.program['offset'] = (-0.5, 0., 0.)
+        #self.program['scale'] = (0.5, 1., 1.)
+        #self.program['offset'] = (-0.5, 0., 0.)
         self.vao.render()
 
+        self.ctx.viewport = (self.window_size[0]//2, 0, self.window_size[0]//2, self.window_size[1])
         self.program['camera'].write(proj*lookr)
-        self.program['scale'] = (0.5, 1., 1.)
-        self.program['offset'] = (0.5, 0., 0.)
+        #self.program['scale'] = (0.5, 1., 1.)
+        #self.program['offset'] = (0.5, 0., 0.)
         self.vao.render()
 
+    def resize(self, width: int, height: int):
+        self.window_size = (width, height)
 
 BasicStereoWindow.run()
