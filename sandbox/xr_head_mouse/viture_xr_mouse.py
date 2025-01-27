cb83b1da01a1020cd7665ef41d38d49c7d56908e
Erik Tollerud <erik.tollerud@gmail.com>
2025-01-26 22:13:51 -0500

    processing concept working

--- sandbox/xr_head_mouse/viture_xr_mouse.py
+++ sandbox/xr_head_mouse/viture_xr_mouse.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import time
 import subprocess
 import selectors
@@ -5,7 +7,17 @@ import selectors
 class FailedReaderError(Exception):
     pass
 
-def main(execpath):
+
+def process_line(line):
+    # wb = line[5:9]
+    # xb = line[9:13]
+    # yb = line[13:17]
+    # zb = line[17:21]
+    buffer_wxyz = line[5:21]
+    wxyz = np.frombuffer(buffer_wxyz, dtype=np.float32)
+    print(wxyz)
+
+def main(execpath, nignore_initial):
 
     process = subprocess.Popen(execpath, stdout=subprocess.PIPE)
 
@@ -22,23 +34,50 @@ def main(execpath):
     sel = selectors.DefaultSelector()
     sel.register(process.stdout, selectors.EVENT_READ)
 
-    lastread = time.time()
-    i = 0
+    fulline = b''
+    completed_lines = 0
+    loops_since_last_processing = 0
+    nzeros = 0
     while True:
-        i += 1
         for key, _ in sel.select(timeout=0):
             line = key.fileobj.readline()
-            nowread = time.time()
-            print('read line with length', len(line), 'w/ delay', (nowread - lastread)*1e3, 'ms, loop runs', i)
-            lastread = nowread
-            i = 0
+            if line.startswith(b'start'):
+                if len(fulline) > 0:
+                    print('reached a start without a completed line! possible error in viture reading process.')
+                fulline = line
+            else:
+                fulline += line
+            if len(fulline) == 22:
+                if not fulline.startswith(b'start'):
+                    print('got a line that does not start with start! Skipping...')
+                else:
+                    completed_lines += 1
+                    if completed_lines <= nignore_initial:
+                        fulline = b''
+                        continue
+                    process_line(fulline)
+                    print('loops overhead:', loops_since_last_processing)
+                    if loops_since_last_processing == 0:
+                        nzeros += 1
+                    else:
+                        nzeros = 0
+                    if nzeros > 1:
+                        print("the viture process may be outpacing python... consecutive immediate reads:", nzeros)
+                    loops_since_last_processing = -1
+                fulline = b''
+            elif len(fulline) > 22:
+                print('line overflowed!')
+                fulline = b''
+        loops_since_last_processing += 1
+
 
 if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--execpath', default='build/viture-logger')
+    parser.add_argument('--nignore-initial', default=120, type=int)
 
     args = parser.parse_args()
 
-    main(args.execpath)
+    main(args.execpath, args.nignore_initial)
